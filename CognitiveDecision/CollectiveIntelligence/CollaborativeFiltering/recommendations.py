18b27b8943bd7533e5e500972ea68436e4d5f5da
Chhorm Chhatra <ch.chhatra@gmail.com>
2017-11-21 23:08:22 +0700

    Recommendations

--- CognitiveDecision/CollectiveIntelligence/CollaborativeFiltering/recommendations.py
+++ CognitiveDecision/CollectiveIntelligence/CollaborativeFiltering/recommendations.py
@@ -34,10 +34,10 @@ critics = {
         'Superman Returns': 4.0
     },
     'Chhatra': {
-        'Harry Potter': 5, 'The Emoji': 5
+        'Harry Potter': 5, 'The Emoji': 5, 'Superman Returns': 4.0, 'You, Me and Dupree': 1.0
     },
     'Chhorm': {
-        'Harry Potter': 5, 'The Emoji': 5
+        'Harry Potter': 5, 'The Emoji': 5, 'Hotel Trans.': 2.5, 'Lion king': 5
     }
 }
 
@@ -62,7 +62,7 @@ def sim_distance(prefs, person1, person2):
     # to make result is between 0 and 1
     # compare result with 1 (divide by 1)
     # to avoid divide by 0, add 1 to the sqrt(result) above
-    return 1/(1 + sqrt(sum_of_squares))
+    return 1 / (1 + sqrt(sum_of_squares))
 
 
 # Pearson Correlation Score
@@ -100,8 +100,76 @@ def sim_pearson(prefs, p1, p2):
     sumxy = sum([prefs[p1][it] * prefs[p2][it] for it in similar])
 
     # score
-    num = n*sumxy - sumx * sumy
-    den = (sqrt(n*sumx_sqr - sumx ** 2))*(sqrt(n*sumy_sqr - sumy ** 2))
+    num = n * sumxy - sumx * sumy
+    den = (sqrt(n * sumx_sqr - sumx ** 2)) * (sqrt(n * sumy_sqr - sumy ** 2))
     if den == 0:
         return 0
     return num / den
+
+
+# Ranking
+# returns the best match for a person
+# number of results and similarity fun() are optional params
+def top_matches(prefs, person, n=5, similarity=sim_pearson):
+    scores = [(similarity(prefs, person, other), other)
+              for other in prefs if other != person]
+    # sort and reverse - top first
+    scores.sort()
+    scores.reverse()
+    return scores[:n]
+
+
+# Recommendations
+# Decision Making Type: Weighted Score
+# https://www.youtube.com/watch?v=FefJ1paq750 && https://www.youtube.com/watch?v=WpLXbV0jm9c
+# In this scenario, to recommend (and rank) a set of movie to a particular users
+# we focus of Weighted Score, in which
+# the similarity scores of the user and the others are the decision making factors (the value is weighting)
+# the movies and the rating scores are the options (value is the score for each)
+# to find out the set of recommendations is to find out the weighted decision
+# however, to improve the accuracy and tendency in which a movie is overrated or underrated
+# we have to compare the real weighted decision score to the overall weighting score
+# set of recommendations = weighted decision score / overall weighting score
+# e.g.
+#
+# (Factors)   (WeightingScore)  -------------------Option Score-----------------------
+# Critic        Similarity      Night   Sim*Night   Lady     Sim*Lady  Luck    Sim*Luck
+# Rose          0.99            3.0     2.97        2.5      2.48      3.0     2.97
+# Seymour       0.38            3.0     1.14        3.0      1.14      1.5     0.57
+# Puig          0.89            4.5     4.02                           3.0     2.68
+# LaSalle       0.92            3.0     2.77        3.0      2.77      2.0     1.85
+# Matthews      0.66            3.0     1.99        3.0      1.99
+# -------------------------------------------------------------------------------------
+# Total                                 12.89                8.38              8.07  <= weighted decision score
+# SSum                                  3.84                 2.95              3.18  <= overall weighting score
+# Total/SSum                            3.35                 2.83              2.53  <= Final Decision
+#
+# KeyTerm:
+# S = Similarity = Sim
+# SSum = Sum of the similarity score of the people who rates the movie
+# program
+def get_recommendations(prefs, person, similarity=sim_pearson):
+    totals = {}
+    ssum = {}
+    for other in prefs:
+        if other == person:
+            continue
+        sim = similarity(prefs, person, other)
+        if sim <= 0:
+            continue
+        for item in prefs[other]:
+            # only movie the person hasn't seen like other
+            if item not in prefs[person] or prefs[person][item] == 0:
+                totals.setdefault(item, 0)
+                totals[item] += prefs[other][item] * sim
+                # sum of similarity or overall weighting score
+                ssum.setdefault(item, 0)
+                ssum[item] += sim
+    # normalized list
+    rankings = [
+        (total / ssum[item], item) for item, total in totals.items()
+    ]
+    # return the sorted list only
+    rankings.sort()
+    rankings.reverse()
+    return rankings
--- CognitiveDecision/CollectiveIntelligence/CollaborativeFiltering/similarity.py
+++ CognitiveDecision/CollectiveIntelligence/CollaborativeFiltering/similarity.py
@@ -22,3 +22,23 @@ pref4 = recommendations.sim_pearson(
     recommendations.critics, 'Lisa Rose', 'Gene Seymour'
 )
 print(pref4)
+
+
+# ranking
+rank = recommendations.top_matches(
+    recommendations.critics, 'Toby', n=4
+)
+print(rank)
+
+# recommendation
+rec1 = recommendations.get_recommendations(
+    recommendations.critics, 'Toby'
+)
+rec2 = recommendations.get_recommendations(
+    recommendations.critics, 'Chhatra'
+)
+rec3 = recommendations.get_recommendations(
+    recommendations.critics, 'Toby', recommendations.sim_distance
+)
+print('The recommendation: ')
+print(rec1, rec2, rec3)
